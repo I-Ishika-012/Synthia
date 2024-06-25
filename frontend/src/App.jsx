96f842b0c65190db9fbae34bb59fe64b2668900c
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-25 20:26:18 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,8 @@
-import { useMutation } from '@tanstack/react-query'
-import React from 'react'
-import axios from 'axios'
+import { useState } from 'react'
+import './App.css'
 
-//function to send message
-const sendMsgAPI = async (msg) => {
-    const res = await axios.post('http://localhost:9090/ask', msg)
-    return res.data
+function App() {
+  
 }
 
-
-const Chat = () => {
-    const [message, setMessage] = useState('')
-    const [isTyping, setIsTyping] = useState(false)
-    const [conversation, setConversation] = useState([{role: 'assistant', content: 'Hello, how can I help you today?'}])
-
-    //useMutation hook to send message
-    const mutation = useMutation({
-        mutationFn: sendMsgAPI, 
-        mutationKey: 'chatbot',
-        onSuccess: (data) => {
-            setConversation((prevConversation) => [...prevConversation, {role: 'assistant', content: data.message}])
-            setMessage('')
-            setIsTyping(false)
-        }
-    })
-
-    //handle submit
-    const handleSubmitMsg = (e) => {
-        e.preventDefault()
-        const currentMsg = message.trim()
-        if(!currentMsg) {alert('Please enter a message'); return}
-        setConversation((prevConversation) => [...prevConversation, {role: 'user', content: currentMsg}])
-        setIsTyping(true)
-        setMessage('')
-        mutation.mutate({message: currentMsg})
-    }
-
-  return (
-    <>
-        <div className="header">
-            <h1 className="title">AI Chatbot</h1>
-        </div>
-    </>
-  )
-}
-
-export default Chat
+export default App
