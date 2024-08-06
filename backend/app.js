edcafc4a979ec86dca755e5400744e3ab0e91812
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-08-06 20:12:03 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,32 @@
 import dotenv from "dotenv";
 import express from "express";
 import OpenAI from "openai";
+import cors from "cors";
 
 dotenv.config();
+
+//?initialize express app
+const PORT = process.env.PORT || 9090;
+const app = express();
+
+//? CORS options for allowing requests from specific origins
+const corsOptions = {
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true, // Important for cookies
+  };
+
+//? enable CORS middleware
+app.use(cors(corsOptions));
+
+
+//? middleware
+app.use(express.json());
 
 //?authenticate with openai using api key
 const openai = new OpenAI({
     apiKey: process.env.OPEN_AI_SECRET_KEY,
 });
 
-//?initialize express app
-const PORT = process.env.PORT || 9090;
-const app = express();
-
-//? middleware
-app.use(express.json());
 
 //?global variables
 let conversationHistory = [{
@@ -29,11 +41,13 @@ app.post("/ask", async(req, res) => {
         const completion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: conversationHistory
-        }).then((res) => {
-            const botResponse = completion.choices[0].message.content;
-            res.json({message: botResponse});
         });
+            const botResponse = completion.choices[0].message.content;
+             // Update conversation history with the assistant's response
+            conversationHistory.push({ role: "assistant", content: botResponse });
+            res.json({message: botResponse});
     } catch (error) {
+        console.error("Error calling OpenAI: ", error);
         res.status(500).send("An error occurred. Please try again later.");
     }
 });
